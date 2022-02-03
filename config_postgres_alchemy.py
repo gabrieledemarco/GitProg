5cde4bd80e3c662d228a0ab1372b8fa0f07fd427
gabrieledemarco <98948019+gabrieledemarco@users.noreply.github.com>
2022-02-03 12:32:36 +0100

    Ci siamo ci siamo

---
+++ config_postgres_alchemy.py
@@ -0,0 +1,5 @@
+postgres_sql = {'user': 'postgres',
+                'password': 'gabriele',
+                'host': 'localhost',
+                'port': 5432,
+                'db': 'Binance'}
---
+++ config_postgress.py
@@ -0,0 +1,5 @@
+host = "localhost"
+database = "Binance"
+user = "postgres"
+password = "gabriele"
+default_fetc_size = 1000
