bcd7702e50463d0167745bfe629dd7010cd92a69
bots93 <mattiabotta06@gmail.com>
2022-02-06 12:15:47 +0100

    add table

--- Update_Table.py
+++ Update_Table.py
@@ -16,3 +16,17 @@ class UpdateTable:
     def dividends_table(self):
         return self.conn.load_table(spark=self.spark, schema="public", name_table="dividends")
 
+    def buy_sell_fiat_table(self):
+        return self.conn.load_table(spark=self.spark, schema="public", name_table="buy_sell_fiat")
+
+    def deposit_withdraw_fiat_table(self):
+        return self.conn.load_table(spark=self.spark, schema="public", name_table="deposit_withdraw_fiat")
+
+    def deposits_crypto_table(self):
+        return self.conn.load_table(spark=self.spark, schema="public", name_table="deposits_crypto")
+
+    def users_table(self):
+        return self.conn.load_table(spark=self.spark, schema="public", name_table="users")
+
+    def withdraw_crypto_table(self):
+        return self.conn.load_table(spark=self.spark, schema="public", name_table="withdraw_crypto")
