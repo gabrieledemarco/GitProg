a344235c05808445a85ebc85c0ed69723bdb5d90
bots93 <mattiabotta06@gmail.com>
2022-02-06 12:20:26 +0100

    change_name_function

--- Update_Table.py
+++ Update_Table.py
@@ -7,26 +7,26 @@ class UpdateTable:
         self.conn = SparkConnection(path="~/Desktop/GitProg/", app_name="Project")
         self.spark = self.conn.connection_spark()
 
-    def orders_table(self):
+    def download_orders(self):
         return self.conn.load_table(spark=self.spark, schema="public", name_table="orders")
 
-    def trades_table(self):
+    def download_trades(self):
         return self.conn.load_table(spark=self.spark, schema="public", name_table="trades")
 
-    def dividends_table(self):
+    def download_dividends(self):
         return self.conn.load_table(spark=self.spark, schema="public", name_table="dividends")
 
-    def buy_sell_fiat_table(self):
+    def download_buy_sell_fiat(self):
         return self.conn.load_table(spark=self.spark, schema="public", name_table="buy_sell_fiat")
 
-    def deposit_withdraw_fiat_table(self):
+    def download_deposit_withdraw_fiat(self):
         return self.conn.load_table(spark=self.spark, schema="public", name_table="deposit_withdraw_fiat")
 
-    def deposits_crypto_table(self):
+    def download_deposits_crypto(self):
         return self.conn.load_table(spark=self.spark, schema="public", name_table="deposits_crypto")
 
-    def users_table(self):
+    def download_users(self):
         return self.conn.load_table(spark=self.spark, schema="public", name_table="users")
 
-    def withdraw_crypto_table(self):
+    def download_withdraw_crypto(self):
         return self.conn.load_table(spark=self.spark, schema="public", name_table="withdraw_crypto")
