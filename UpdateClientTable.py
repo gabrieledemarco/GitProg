26ba16e292345b8593293d36e94a7c64989076c8
bots93 <mattiabotta06@gmail.com>
2022-02-06 19:31:55 +0100

    update_div

--- UpdateClientTable.py
+++ UpdateClientTable.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import pyspark.sql.functions as F
+from tqdm import tqdm
 
 from BinanceService import BinanceService
 from DbService import DbService
@@ -16,19 +17,44 @@ class UpdateClientTable:
         self.spark_ser = SparkToDBService()
         self.users = self.spark_ser.download_users()
 
-    def update_dividends(self):
+    def last_update_date(self, name_table_update):
 
-        update_date = self.db.get_select_with_where(select_columns='update_date', name_table='update_table',
-                                                    where_columns='name_table', values_column='dividends')
-        new_users = self.users.select("api_key", "api_secret", "registration_date")\
-                        .where(F.col("registration_date") > update_date)
-        old_users = self.users.select("api_key", "api_secret", "registration_date")\
-                        .where(F.col("registration_date") < update_date)
+        return self.db.get_select_with_where(select_columns='update_date', name_table='update_table',
+                                                    where_columns='name_table', values_column=name_table_update)
 
-        for new_client in new_users.rdd.collect():
-            ser_bin = BinanceService(api_key=new_client[0], api_secret=new_client[1])
-            ins_tab = InsertValueInTable(api_key=new_client[0], api_secret=new_client[1])
-            limit_div = (datetime.now().date() - update_date.date()).days
+    def get_new_users(self, update_date: datetime):
+
+        return self.users.select("api_key", "api_secret", "registration_date")\
+                    .where(F.col("registration_date") > update_date)
+
+    def get_old_users(self, update_date: datetime):
+        return self.users.select("api_key", "api_secret", "registration_date")\
+                    .where(F.col("registration_date") < update_date)
+
+    def update_dividends(self, users, type_users: str):
+
+        update_date = self.last_update_date(name_table_update="dividends")
+
+        for user in users.rdd.collect():
+            ser_bin = BinanceService(api_key=user[0], api_secret=user[1])
+            ins_tab = InsertValueInTable(api_key=user[0], api_secret=user[1])
+            if type_users == "old":
+                limit_div = (datetime.now().date() - update_date.date()).days
+            else:
+                date_registration = self.db.get_select_with_where(select_columns="registration_date",
+                                                                  name_table="users", where_columns="api_key",
+                                                                  values_column=user[0])
+                limit_div = (datetime.now().date() - date_registration.date()).days
+
+            all_div = []
+            asset_tot = self.db.get_all_value_in_column(name_column="coin", name_table="crypto")
+            for asset in tqdm(asset_tot, desc="Dividends's table upsert"):
+                dividends = ser_bin.get_daily_div_history(asset=asset, limit=limit_div)
+                if dividends:
+                    for dividend in dividends:
+                        all_div.append((dividend['id'], dividend['tranId'], dividend['asset'], dividend['amount'],
+                                        dividend['divTime'], dividend['enInfo']))
+
 
 
 
