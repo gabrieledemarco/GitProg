4da324396ad4b34ec0df7f599702a806ab3f925f
gabrieledemarco <98948019+gabrieledemarco@users.noreply.github.com>
2022-02-08 09:00:08 +0100

    I have add some info message at authentication Login

--- SideLog.py
+++ SideLog.py
@@ -57,6 +57,13 @@ def Log_in_form(passwords: list, names: list, usernames: list):
             authenticator = stauth.authenticate(names, usernames, hashed_passwords,
                                                 'some_cookie_name', 'some_signature_key', cookie_expiry_days=30)
             name, authentication_status = authenticator.login('Login', 'main')
+
+            if authentication_status is False:
+                st.error("Username or Password are incorrect")
+            elif authentication_status is True:
+                st.success(f"User Authorized, welcome dear {name} ")
+            elif authentication_status is None:
+                st.info("Please access to your account with a Username and Password")
         except Exception:
             print("")
         return
